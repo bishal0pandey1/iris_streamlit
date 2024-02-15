61ac5aebcc3f68cb943a73da2a9517f4a0c5e412
bishal0pandey1 <pandu216bishu@gmail.com>
2024-02-15 11:34:02 +0530

    some changes to requirements.txt

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ col1,col2=st.columns(2)
 with col1:
     st.text("Sepal Characteristics")
     sepal_l=st.slider('Sepal length (cm)',1.0,8.0,0.5)
-    sepal_w=st.slider('Sepal width(cm)'2.0,4.4,0.5)
+    sepal_w=st.slider('Sepal width(cm)',2.0,4.4,0.5)
 
 with col2:
      st.text("Petal Characteristics")
      petal_l=st.slider('Petal length (cm)',1.0,7.0,0.5)
-     petal_w=st.slider('Petal width(cm)'0.1,2.5,0.5)
+     petal_w=st.slider('Petal width(cm)',0.1,2.5,0.5)
 
 st.text('')
-if st.buttom('Predict type of Iris:'):
+if st.button('Predict type of Iris:'):
      result=predict(
           np.array([[sepal_l,sepal_w,petal_l,petal_w]]))
      st.text(result[0])
